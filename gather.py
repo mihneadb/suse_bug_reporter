f950787ba5f68a3fd3b3296f218848855fabad18
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-05-26 19:19:34 +0300

    some indentation

--- gather.py
+++ gather.py
@@ -22,28 +22,28 @@ def gather(release=1, uname=1, cpu=1, lspci=1, lsmod=1):
     os.system('uname -a >> %s' % sysinfo_file_name)
     sysinfo_file = open('%s' % sysinfo_file_name, 'a')
     sysinfo_file.write('\n------------------------------------------' \
-        '-------------------------------------\n\n')
+            '-------------------------------------\n\n')
 
     sysinfo_file.write('CPU info:\n\n')
     sysinfo_file.close()
     os.system('cat /proc/cpuinfo >> %s' % sysinfo_file_name)
     sysinfo_file = open('%s' % sysinfo_file_name, 'a')
     sysinfo_file.write('\n------------------------------------------' \
-        '-------------------------------------\n\n')
+            '-------------------------------------\n\n')
 
     sysinfo_file.write('lspci output:\n\n')
     sysinfo_file.close()
     os.system('lspci >> %s' % sysinfo_file_name)
     sysinfo_file = open('%s' % sysinfo_file_name, 'a')
     sysinfo_file.write('\n------------------------------------------' \
-        '-------------------------------------\n\n')
+            '-------------------------------------\n\n')
 
     sysinfo_file.write('lsmod output:\n\n')
     sysinfo_file.close()
     os.system('lsmod >> %s' % sysinfo_file_name)
     sysinfo_file = open('%s' % sysinfo_file_name, 'a')
     sysinfo_file.write('\n------------------------------------------' \
-        '-------------------------------------\n\n')
+            '-------------------------------------\n\n')
 
     sysinfo_file.close()
 
