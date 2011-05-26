b122dc52c7f96b8702f07eb8165d12eb666cc7ec
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-05-26 19:05:46 +0300

    corrected file close/open to sync the data on the hdd

--- gather.py
+++ gather.py
@@ -2,24 +2,45 @@
 
 import os
 
+# you can define the output file name here
 sysinfo_file_name = 'sysinfo'
+
+
 sysinfo_file = open('%s' % sysinfo_file_name, 'w')
 
-sysinfo_file.write('OS version:\n')
-os.system('cat /etc/SuSE-release > %s' % sysinfo_file_name)
-sysinfo_file.write('\n------------------------------------------\n')
+sysinfo_file.write('OS version:\n\n')
+sysinfo_file.close()
+os.system('cat /etc/SuSE-release >> %s' % sysinfo_file_name)
+sysinfo_file = open('%s' % sysinfo_file_name, 'a')
+sysinfo_file.write('\n------------------------------------------' \
+        '-------------------------------------\n\n')
 
-sysinfo_file.write('CPU info:\n')
-os.system('cat /proc/cpuinfo > %s' % sysinfo_file_name)
-sysinfo_file.write('\n------------------------------------------\n')
+sysinfo_file.write('Uname output:\n\n')
+sysinfo_file.close()
+os.system('uname -a >> %s' % sysinfo_file_name)
+sysinfo_file = open('%s' % sysinfo_file_name, 'a')
+sysinfo_file.write('\n------------------------------------------' \
+        '-------------------------------------\n\n')
 
-sysinfo_file.write('lspci output:\n')
-os.system('lspci > %s' % sysinfo_file_name)
-sysinfo_file.write('\n------------------------------------------\n')
+sysinfo_file.write('CPU info:\n\n')
+sysinfo_file.close()
+os.system('cat /proc/cpuinfo >> %s' % sysinfo_file_name)
+sysinfo_file = open('%s' % sysinfo_file_name, 'a')
+sysinfo_file.write('\n------------------------------------------' \
+        '-------------------------------------\n\n')
 
-sysinfo_file.write('lsmod output:\n')
-os.system('lsmod > %s' % sysinfo_file_name)
-sysinfo_file.write('\n------------------------------------------\n')
+sysinfo_file.write('lspci output:\n\n')
+sysinfo_file.close()
+os.system('lspci >> %s' % sysinfo_file_name)
+sysinfo_file = open('%s' % sysinfo_file_name, 'a')
+sysinfo_file.write('\n------------------------------------------' \
+        '-------------------------------------\n\n')
 
+sysinfo_file.write('lsmod output:\n\n')
+sysinfo_file.close()
+os.system('lsmod >> %s' % sysinfo_file_name)
+sysinfo_file = open('%s' % sysinfo_file_name, 'a')
+sysinfo_file.write('\n------------------------------------------' \
+        '-------------------------------------\n\n')
 
 sysinfo_file.close()
