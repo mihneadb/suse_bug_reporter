fa6d03e0d6ed85adc636bd6e9d10e0e243866d0e
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-06-01 15:04:29 +0300

    uname module

--- lsmod.py
+++ lsmod.py
@@ -4,19 +4,22 @@ from subprocess import Popen, PIPE
 
 
 def gather_from_lsmod():
-    ''' Generates a list of strings which represent the modules loaded
-        at the moment '''
+    ''' Generates a dictionary with the key modules and value a list of strings
+        which represent the modules loaded at the moment '''
+
+    modules_list = list()
+    modules = dict()
 
     output = Popen('lsmod', stdout=PIPE).communicate()[0]
     output = output.splitlines()
     del output[0]
 
-    modules = list()
-
     for line in output:
-        modules.append(line.split()[0])
+        modules_list.append(line.split()[0])
 
+    modules['modules'] = modules_list
     return modules
+
 
 if __name__ == '__main__':
     modules = gather_from_lsmod()
