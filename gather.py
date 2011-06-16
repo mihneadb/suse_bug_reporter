55733132b8e6da5d3dccd3e8dcea6b60a75c2af1
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-06-16 11:54:03 +0300

    added gather boot arguments

--- gather.py
+++ gather.py
@@ -3,7 +3,7 @@
 from subprocess import Popen, PIPE
 
 # where to gather the information from
-gather_from = ['lsmod', 'uname', 'release', 'cpu', 'vga', 'mem']
+gather_from = ['lsmod', 'uname', 'release', 'cpu', 'vga', 'mem', 'cmdline']
 
 def gather_data(gather_list):
     ''' returns a dictionary with keys the sources from where the data was
