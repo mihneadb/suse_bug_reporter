eed299db410342f4139014aa4dd45fd6fb5210a4
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-06-19 16:24:25 +0300

    sample code for bugzilla interaction

---
+++ interact.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import bugzilla
+from getpass import getpass
+
+bugzillaURL = 'https://bugzilla.novell.com/xmlrpc.cgi'
+
+
+user = raw_input('Insert user name: ')
+password = getpass('Insert password: ')
+
+
+cls = bugzilla.getBugzillaClassForURL(bugzillaURL)
+bz = cls(url=bugzillaURL, user=user, password=password)
+
+test = bz.query({'summary': 'python'})
+print len(test)
