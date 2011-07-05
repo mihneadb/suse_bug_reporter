f435e9c71e6aa3d8cb63283e876d00e326540b4d
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-07-05 14:33:32 +0300

    edit message function

--- util/console.py
+++ util/console.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+
+import os
+import subprocess
+import sys
+import tempfile
+
+
 def print_list(a_list, attr=None, columns=1, msg=None):
     ''' formats the options list on one or two columns with an index
     associated to each entry;
@@ -59,3 +66,68 @@ def get_index(length, msg=None):
             break
 
     return idx
+
+
+def custom_input(msg='', preselect=''):
+    ans = raw_input(msg)
+    if ans == '':
+        return preselect
+
+    return ans
+
+
+def get_editor():
+    ''' Author: Michal Vyskocil <mvyskocil@suse.cz> '''
+    default = 'vim'
+    if sys.platform[:3] == 'win':
+        default = 'notepad'
+
+    return os.getenv('EDITOR', default=default)
+
+
+def edit_message(template, prefix="", resp=None):
+    ''' Author: Michal Vyskocil <mvyskocil@suse.cz> '''
+    if resp:    return prefix + resp
+    
+    delim = '#Write a description below.'
+
+    editor = get_editor()
+
+    (fd, filename) = tempfile.mkstemp(prefix = 'susereport-description', suffix = '.txt', dir = '/tmp')
+    f = os.fdopen(fd, 'w')
+    if template != '':
+        f.write(template)
+    f.write('\n\n')
+    if prefix != '':
+        f.write(prefix)
+    f.write('\n\n')
+    f.write(delim)
+    f.write('\n\n')
+    f.flush()
+    f.close()
+    mtime_orig = os.stat(filename).st_mtime
+
+    line = len(template.split('\n')) + len(prefix.split('\n'))+4
+    while 1:
+        subprocess.call('%s +%d %s' % (editor, line, filename), shell=True)
+        mtime = os.stat(filename).st_mtime
+        if mtime_orig < mtime:
+            msg = open(filename).read()
+            os.unlink(filename)
+
+            lmsg = msg.split(delim)
+            if len(lmsg) > 1:
+                return prefix + '\n' + lmsg[1].lstrip()
+            return prefix + '\n\n' + msg
+
+        else:
+            input = raw_input('Log message unchanged or not specified\n'
+                              'a)bort, c)ontinue, e)dit: ', 'a')
+            if input in 'aA':
+                os.unlink(filename)
+                sys.exit(1)
+            elif input in 'cC':
+                os.unlink(filename)
+                return ''
+            elif input in 'eE':
+                pass
