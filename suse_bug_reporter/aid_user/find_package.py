5ee93971627fa32e9f4512a2f6ad694053e5e85e
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-07-26 13:43:22 +0300

    modularized

--- suse_bug_reporter/aid_user/find_package.py
+++ suse_bug_reporter/aid_user/find_package.py
@@ -1,36 +1,53 @@
 #!/usr/bin/env python
 
+import os.path
 import rpm
 
-from subprocess import Popen, PIPE
 
-def find_package(pr=True):
+def which(program):
+    ''' returns absolute path to program's executable, or None if it's not found '''
+
+    PATH = ('/bin', '/sbin', '/usr/bin', '/usr/sbin')
+
+    for d in PATH:
+        abs_path = os.path.join(d, program)
+        if os.path.exists(abs_path):
+            return abs_path
+    return None
+
+
+def get_package(abs_path):
+    ''' returns the package (if it exists) for the executable found at the
+        abs path given. '''
+    assert os.path.isabs(abs_path)
+
+    ts = rpm.TransactionSet()
+    match = ts.dbMatch('basenames', abs_path)
+
+    pkg = None
+    for header in match:
+        pkg = header['name']
+
+    return pkg
+
+
+def find_package():
 
     print "Which binary's package do you want to find?"
+    print 'You can enter either its name or its absolute path.'
     name = raw_input('--> ')
     name = name.strip()
 
-    ts = rpm.TransactionSet()
     
-    if '/' not in name:
-        output = Popen(('which', name), stdout=PIPE, stderr=PIPE).communicate()
-
-        if output[0] == '':
-            print 'Nothing named %s was found in your $PATH. '\
-                'Maybe add /sbin and /usr/sbin to it?' % name
+    # check if this is absolute path
+    if not os.path.isabs(name):
+        name = which(name)
+        if name == None:
             return None
 
-        name = output[0].strip()
-
-    match = ts.dbMatch('basenames', name)
-    for header in match:
-        pkg = header['name']
+    pkg = get_package(name)
     
-    if pr:
-        if pkg != '' and pkg != None:
-            msg = 'The package that gives %s is %s.' % (name, pkg)
-            print msg
-        else:
-            print 'No package found.'
-    else:
-        return pkg
+    if pkg == None:
+        return None
+
+    return 'The package for %s is %s.' % (name, pkg)
