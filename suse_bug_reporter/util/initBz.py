0f5fa02c5cf56ef703c1096acfc2bfdcaf392eb5
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-07-28 13:33:22 +0300

    moved from bin/susebugreporter.py

---
+++ suse_bug_reporter/util/initBz.py
@@ -0,0 +1,15 @@
+import bugzilla
+from suse_bug_reporter.util import login
+
+def initBugzilla():
+    #check login
+    (username, password) = login.getCreds()
+    
+    print "Connecting to Novell's Bugzilla..."
+
+    #instantiate the bz object
+    bugzillaURL = 'https://bugzilla.novell.com/xmlrpc.cgi'
+    cls = bugzilla.getBugzillaClassForURL(bugzillaURL)
+    bz = cls(url=bugzillaURL, user=username, password=password)
+
+    return bz
