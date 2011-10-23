fbb40b6f49557ad2ebcfd6eed9faad0cc60cb679
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2011-10-23 22:09:29 +0300

    Add GPLv2+ license.

--- bugreporter/util/FSM_def.py
+++ bugreporter/util/FSM_def.py
@@ -1,3 +1,21 @@
+'''
+Copyright (C) 2011  Mihnea Dobrescu-Balaur
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 # FSM_def.py - a class definition module for suse bug reporting tool for the
 # Finite State Machine used to get bug data from the user
 #
@@ -9,6 +27,7 @@
 # Free Software Foundation; either version 2 of the License, or (at your
 # option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
 # the full text of the license.
+'''
 
 __version__ = '0.1'
 
